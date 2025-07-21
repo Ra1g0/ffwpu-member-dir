cc3957ec99d8307c6a6d7ec83513423c80544474
Sybil0g <smsenriquez00@gmail.com>
2025-07-21 15:43:09 +0800

    login

--- frontend_member/src/App.tsx
+++ frontend_member/src/App.tsx
@@ -4,7 +4,7 @@ import LoginPage from './app/(private)/login/page'
 import DashboardPage from './app/(private)/dashboard/page'
 import MemberPage from './app/(private)/member/page'
 import ForgotPassword from './app/(private)/login/forgatpass/page'
-import PrivateRoute from './components/PrivateRoute';
+//import PrivateRoute from './components/PrivateRoute';
 
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
-      <Route path="/member" element={<PrivateRoute><MemberPage /></PrivateRoute>} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/member" element={<MemberPage />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
     </Routes> 
   )
